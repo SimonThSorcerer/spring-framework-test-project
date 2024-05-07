183aad82f875b522ae233a6a0ea124bd5c3a3b76
Simon András Péter <simonandraspeter@gmail.com>
2024-05-07 22:30:11 +0200

    Creating a @RestController to connect with Angular

--- src/main/java/com/springbootproject/controller/StudentRestController.java
+++ src/main/java/com/springbootproject/controller/StudentRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping("/student")
+@RequestMapping("/studentrest")
 public class StudentRestController {
     @Autowired
     StudentServiceImpl studentServiceImpl;
