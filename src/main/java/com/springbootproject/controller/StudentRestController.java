c2f15761dcf54da1e6571d433c278445f3f50f41
Simon András Péter <simonandraspeter@gmail.com>
2024-05-07 23:29:52 +0200

    Cross Origin CORS update

--- src/main/java/com/springbootproject/controller/StudentRestController.java
+++ src/main/java/com/springbootproject/controller/StudentRestController.java
@@ -4,14 +4,12 @@ import com.springbootproject.object.Student;
 import com.springbootproject.service.StudentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequestMapping("/studentrest")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentRestController {
     @Autowired
     StudentServiceImpl studentServiceImpl;
